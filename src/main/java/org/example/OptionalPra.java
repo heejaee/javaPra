93e9ce711d9258fa70216ac98771b23e6bdc5708
hee jae <hjhjhjhj3770@naver.com>
2023-08-23 14:40:18 +0900

    Optional 연습

---
+++ src/main/java/org/example/OptionalPra.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class OptionalPra {
+
+    public static void main(String[] args) {
+        Optional<String> op = Optional.ofNullable("123");
+        //String s = op.orElseGet(() -> "없음");
+        //String s =op.orElseThrow(NullPointerException::new);
+
+        op.ifPresent((s)->System.out.println(s));
+
+
+        String str = "abc";
+        Optional<String> optVal = Optional.of(str);
+
+        System.out.println(IntStream.range(0, 10).max());
+    }
+}
